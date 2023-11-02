a69dcd25588913f7437575e4b2c990e1d67d96a5
Carlota <carlotabergel7@hotmail.com>
2023-11-02 20:08:44 +0100

    1

---
+++ buclesFor.js
@@ -0,0 +1,47 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.add = void 0;
+var myArr1 = ["Anillo", "Collar", "Pulsera"];
+var arrColor = ["verde", "rojo", "azul", "rosa"];
+evenNumbers(10);
+myRever(myArr1);
+isRainbow2(arrColor);
+//console.log(add(arrColor));
+function evenNumbers(num) {
+    for (var i = 1; i < num; i++) {
+        if (i % 2 != 0) {
+            console.log(i);
+        }
+    }
+}
+function myRever(myArr) {
+    for (var i = myArr.length - 1; i >= 0; i--) {
+        console.log(myArr[i]);
+    }
+    return myArr;
+}
+function isRainbow2(myWords) {
+    for (var i = 0; i < myWords.length; i++) {
+        switch (myWords[i]) {
+            case 'verde':
+            case 'violeta':
+            case 'naranja':
+            case 'azul':
+            case 'rojo':
+            case 'amarillo':
+                console.log(myWords[i] + ", si es del arcoiris");
+                break;
+            default:
+                console.log(myWords[i] + ", no es del arcoiris");
+                break;
+        }
+    }
+}
+function add(myWords) {
+    var sumCarac = 0;
+    for (var i = 0; i < myWords.length; i++) {
+        sumCarac += myWords[i].length;
+    }
+    return sumCarac;
+}
+exports.add = add;
---
+++ invocacion.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var buclesFor_1 = require("./buclesFor");
+var arr = ["anillo", "pulsera", "pendientes"];
+var numeroCaracteresArr = (0, buclesFor_1.add)(arr);
+console.log(numeroCaracteresArr);
+// console.log(isEven(numeroCaracteresArr));
