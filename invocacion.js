c13f32cd8f37653b14fffb23dda681084e34aa6f
Carlota <carlotabergel7@hotmail.com>
2023-11-02 20:06:53 +0100

    2

--- invocacion.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var buclesFor_1 = require("./buclesFor");
-var arr = ["anillo", "pulsera", "pendientes"];
-var numeroCaracteresArr = (0, buclesFor_1.add)(arr);
-console.log(numeroCaracteresArr);
-// console.log(isEven(numeroCaracteresArr));
